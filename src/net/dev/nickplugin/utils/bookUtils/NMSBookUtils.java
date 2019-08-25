41d68a46c2fa27816ee253a9f325f3e15c022dbf
JustixDevelopment <justix.dev@gmx.de>
2019-08-25 18:36:51 +0200

    1.224

--- src/net/dev/nickplugin/utils/bookUtils/NMSBookUtils.java
+++ src/net/dev/nickplugin/utils/bookUtils/NMSBookUtils.java
@@ -18,7 +18,7 @@ public class NMSBookUtils extends ReflectUtils {
 		try {
 			Object entityPlayer = p.getClass().getMethod("getHandle").invoke(p);
 			Class<?> craftItemStackClass = getCraftClass("inventory.CraftItemStack");
-			Object craftItemStack = craftItemStackClass.getMethod("asNMSCopy", ItemStack.class).invoke(craftItemStackClass, book);
+			Object nmsItemStack = craftItemStackClass.getMethod("asNMSCopy", ItemStack.class).invoke(craftItemStackClass, book);
 			
 			p.setItemInHand(book);
 			
@@ -29,7 +29,7 @@ public class NMSBookUtils extends ReflectUtils {
 				if(Bukkit.getVersion().contains("1.14.4")) {
 					Class<?> itemWrittenBook = getNMSClass("ItemWrittenBook");
 					
-					if ((boolean) itemWrittenBook.getMethod("a", craftItemStackClass, getNMSClass("CommandListenerWrapper"), entityPlayer.getClass()).invoke(itemWrittenBook, craftItemStack, entityPlayer.getClass().getMethod("getCommandListener").invoke(entityPlayer), entityPlayer)) {
+					if ((boolean) itemWrittenBook.getMethod("a", getNMSClass("ItemStack"), getNMSClass("CommandListenerWrapper"), entityPlayer.getClass()).invoke(itemWrittenBook, nmsItemStack, entityPlayer.getClass().getMethod("getCommandListener").invoke(entityPlayer), entityPlayer)) {
 						Object activeContainer = entityPlayer.getClass().getField("activeContainer").get(entityPlayer);
 						
 		                activeContainer.getClass().getMethod("c").invoke(activeContainer);
@@ -39,7 +39,7 @@ public class NMSBookUtils extends ReflectUtils {
 					Object playerConnection = entityPlayer.getClass().getField("playerConnection").get(entityPlayer);
 					playerConnection.getClass().getMethod("sendPacket", getNMSClass("Packet")).invoke(playerConnection, packet);
 				} else {
-					entityPlayer.getClass().getMethod("a", getNMSClass("ItemStack"), enumHand).invoke(entityPlayer, craftItemStack, mainHand);
+					entityPlayer.getClass().getMethod("a", getNMSClass("ItemStack"), enumHand).invoke(entityPlayer, nmsItemStack, mainHand);
 				}
 			} else {
 				Object packet = getNMSClass("PacketPlayOutCustomPayload").getConstructor(String.class, getNMSClass("PacketDataSerializer")).newInstance("MC|BOpen", getNMSClass("PacketDataSerializer").getConstructor(ByteBuf.class).newInstance(Unpooled.buffer()));
