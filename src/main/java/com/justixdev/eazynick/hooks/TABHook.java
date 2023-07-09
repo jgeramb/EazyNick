ea6540465b381f3252897772f2c9d02781d98334
JustixDevelopment <justix.dev@gmx.de>
2023-07-09 18:49:55 +0200

    fixed TAB support

--- src/main/java/com/justixdev/eazynick/hooks/TABHook.java
+++ src/main/java/com/justixdev/eazynick/hooks/TABHook.java
@@ -2,8 +2,8 @@ package com.justixdev.eazynick.hooks;
 
 import me.neznamy.tab.api.TabAPI;
 import me.neznamy.tab.api.TabPlayer;
-import me.neznamy.tab.api.TablistFormatManager;
-import me.neznamy.tab.api.team.TeamManager;
+import me.neznamy.tab.api.nametag.NameTagManager;
+import me.neznamy.tab.api.tablist.TabListFormatManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -13,29 +13,29 @@ public class TABHook {
 
     private static TabAPI api;
 
-    private final TeamManager teamManager;
-    private final TablistFormatManager tablistFormatManager;
     private final TabPlayer tabPlayer;
+    private final TabListFormatManager tablistFormatManager;
+    private final NameTagManager nametagManager;
 
     public TABHook(Player player) {
         if(api == null)
             api = TabAPI.getInstance();
 
-        this.teamManager = api.getTeamManager();
-        this.tablistFormatManager = api.getTablistFormatManager();
         this.tabPlayer = api.getPlayer(player.getUniqueId());
+        this.nametagManager = api.getNameTagManager();
+        this.tablistFormatManager = api.getTabListFormatManager();
 
-        if((this.teamManager == null) || (this.tablistFormatManager == null) || (this.tabPlayer == null))
+        if((this.tabPlayer == null) || (this.tablistFormatManager == null) || (this.nametagManager == null))
             Bukkit.getLogger().log(Level.SEVERE, "Could not load TabPlayer for '" + player.getName() + "'");
     }
 
     public void update(String tabPrefix, String tabSuffix, String tagPrefix, String tagSuffix, String groupName) {
-        if((this.teamManager == null) || (this.tablistFormatManager == null) || (this.tabPlayer == null))
+        if((this.tabPlayer == null) || (this.tablistFormatManager == null) || (this.nametagManager == null))
             return;
 
         // Set temporarily nametag values
-        this.teamManager.setPrefix(this.tabPlayer, tagPrefix);
-        this.teamManager.setSuffix(this.tabPlayer, tagSuffix);
+        this.nametagManager.setPrefix(this.tabPlayer, tagPrefix);
+        this.nametagManager.setSuffix(this.tabPlayer, tagSuffix);
 
         // Set temporarily list values
         this.tablistFormatManager.setPrefix(this.tabPlayer, tabPrefix);
@@ -46,19 +46,19 @@ public class TABHook {
     }
 
     public void reset() {
-        if((this.teamManager == null) || (this.tablistFormatManager == null) || (this.tabPlayer == null))
+        if((this.tabPlayer == null) || (this.tablistFormatManager == null) || (this.nametagManager == null))
             return;
 
         // Unset temporarily nametag values
-        this.teamManager.resetPrefix(this.tabPlayer);
-        this.teamManager.resetSuffix(this.tabPlayer);
+        this.nametagManager.setPrefix(this.tabPlayer, this.nametagManager.getOriginalPrefix(this.tabPlayer));
+        this.nametagManager.setSuffix(this.tabPlayer, this.nametagManager.getOriginalSuffix(this.tabPlayer));
 
         // Unset temporarily list values
-        this.tablistFormatManager.resetPrefix(this.tabPlayer);
-        this.tablistFormatManager.resetSuffix(this.tabPlayer);
+        this.tablistFormatManager.setPrefix(this.tabPlayer, this.tablistFormatManager.getOriginalPrefix(this.tabPlayer));
+        this.tablistFormatManager.setSuffix(this.tabPlayer, this.tablistFormatManager.getOriginalSuffix(this.tabPlayer));
 
         // Reset group name
-        this.tabPlayer.resetTemporaryGroup();
+        this.tabPlayer.setTemporaryGroup(null);
     }
 
 }
